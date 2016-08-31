82b8413a948713a2ce4e1fe04340facb6ef0c64f
teeppope <terraepop@gmail.com>
2016-08-31 11:56:11 -0700

    Remove unneeded fonts

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,75 +6,67 @@ $(document).ready(function(){
 		event.preventDefault();
 
 	//Declaring variables
-		var nytSection = $('option:selected').val();
-		var url = "https://api.nytimes.com/svc/topstories/v2/"+ nytSection +".json";
-			url += '?' + $.param({'api-key': "140e5fd4387e4761a497e2670d92b49d"});
-		var articleItem="";
-		var $articleGrid = $('.article-grid');
-		var gif = './dev/assets/images/ajax-loader.gif';	
-		var loadingGif = '<li class="loading">';
-			loadingGif += 	'<img src="'+ gif +'" alt="Page Loader">';
-			loadingGif += '</li>';
-		var $articles = $('.articles');
+	var nytSection = $('option:selected').val();
+	var url = "https://api.nytimes.com/svc/topstories/v2/"+ nytSection +".json";
+		url += '?' + $.param({'api-key': "140e5fd4387e4761a497e2670d92b49d"});
+	var articleItem= "";
+	var $articleGrid = $('.article-grid');
+	
+	var gif = './build/assets/images/ajax-loader.gif';	
+	var loadingGif = '<li class="loading">';
+		loadingGif += 	'<img src="'+ gif +'" alt="Page Loader">';
+		loadingGif += '</li>';
+	var $articles = $('.articles');
 
-//Removing the articles before getting new ones
-		$articleGrid.empty();
-		
+	//Removing the articles before getting new ones
+	$articleGrid.empty();
+
 	//Moving the header items to the top
-		$('header').addClass('header-shrink');
+	$('header').addClass('header-shrink');
 	// Loading gif
-		$articleGrid.append(loadingGif);
+	$articleGrid.append(loadingGif);
 
-		
+
 	// Calling NYT API	
-		$.ajax({
-			method: 'GET',
-			url: url,
-			dataType: 'json',
-		})
-		.done(function(data) {
+	$.ajax({
+		method: 'GET',
+		url: url,
+		dataType: 'json',
+	})
+	.done(function(data) {
 
-	
-			var articleObject = data.results;
-			var i = 0;			  		
-			
-			  $.each(articleObject, function(index, value){
-				
-				if(value.multimedia.length && i < 12){
-				  	var articleAbstract= value.abstract,
-				  		articleLink= value.url;
-			
-				  		articleItem = '<li class="indv-article">';
-						articleItem +=	'<a href="'+articleLink+'">';
-						articleItem +=		'<div class="article-bkgrnd" style="background-image: url('+value.multimedia[4].url+')">';
-						articleItem +=			'<div class="abstract">';
-						articleItem +=				'<p>';
-						articleItem +=					articleAbstract;
-						articleItem +=				'</p>';
-						articleItem +=			'</div>';
-						articleItem +=		'</div>';
-						articleItem += 	'</a>';
-						articleItem += '</li>';	
-						i++;
-				  
+		var articleObject = data.results;
+		var i = 0;			  		
 
-					 $articleGrid.append(articleItem);
-			  			
-			  			} // If bracket multimedia brackets
-			  	
-		   	}); // each brackits
-		
-		}) // .done brackets
-		.fail(function(error){
-			$articles.append('<p class="error">Sorry! Something went wrong! Please choose a new section to read!</p>');
+		$.each(articleObject, function(index, value){
 
+			if(value.multimedia.length && i < 12){
+				var articleAbstract= value.abstract,
+					articleLink= value.url;
+
+					articleItem = '<li class="indv-article">';
+					articleItem +=	'<a href="'+articleLink+'">';
+					articleItem +=		'<div class="article-bkgrnd" style="background-image: url('+value.multimedia[4].url+')">';
+					articleItem +=			'<div class="abstract">';
+					articleItem +=				'<p>';
+					articleItem +=					articleAbstract;
+					articleItem +=				'</p>';
+					articleItem +=			'</div>';
+					articleItem +=		'</div>';
+					articleItem += 	'</a>';
+					articleItem += '</li>';	
+					i++;
+
+				$articleGrid.append(articleItem);
+			} // If bracket multimedia brackets
+		}); // each brackits
+	}) // .done brackets
+	.fail(function(error){
+		$articles.append('<p class="error">Sorry! Something went wrong! Please choose a new section to read!</p>');
 		}) //.fail Brackets
-		.always(function(){
-			$('.loading').remove();
+	.always(function(){
+		$('.loading').remove();
 		}); // .always brackets
-
-		$('.error').remove();
-	
+	$('.error').remove();
 	}); //on change brackets
-
 }); //Document Ready brackits
